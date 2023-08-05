9926e865a85c4bf31ccbe8260e23aaf0c9606fa2
SakkiWakki <trashyiiiiiii@gmail.com>
2023-08-05 18:10:32 -0400

    Added the reload command

--- nanobot/__main__.py
+++ nanobot/__main__.py
@@ -1,20 +1,19 @@
+# Env
 from dotenv import load_dotenv
 import os
 
-# bot.py
+# Discord
 import discord
+from discord.ext import commands
 import logging
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 logging.basicConfig(level=logging.INFO)
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Bot(intents=intents)
-
 
 @bot.event
 async def on_ready():
@@ -28,6 +27,18 @@ async def on_message(message):
 
     if message.content.startswith('727'):
         await message.channel.send('WYSI!')
+
+
+@bot.command(name='reload', hidden=True, description="Reloads the bot")
+@commands.is_owner()
+async def reload_cog(ctx):
+    bot.reload_extension('commands.auth')
+    await ctx.respond('Cogs reloaded!', ephemeral=True)
+
+@reload_cog.error
+async def reload_cog_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send('You are not the Yuck.')
 
 bot.load_extension('commands.auth')
 
